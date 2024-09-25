81c366c31d42595be1bdd4099ad24a80ff2d6353
dla <dla@axon.com>
2024-09-25 15:25:58 +0700

    Add copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -35,4 +35,23 @@ return {
       vim.g.workspace_autosave = 0 -- disable auto save on InsertLeave
     end,
   },
+  {
+    'github/copilot.vim',
+    opts = {},
+    config = function() end,
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
