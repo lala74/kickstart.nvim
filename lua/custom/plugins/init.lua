fde01f3bd2793c79a32845dfcde02e4830855739
dla <dla@axon.com>
2024-10-02 14:12:31 +0700

    Refactor

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -12,6 +12,9 @@ local function create_plugin(plugin_name)
 end
 
 return {
+  -- Colorscheme
+  create_plugin 'ellisonleao/gruvbox.nvim',
+
   -- Git plugin
   create_plugin 'tpope/vim-fugitive', -- :Git blame, ...
   create_plugin 'tpope/vim-rhubarb', -- :GBrowse - to open commit url in browser
@@ -20,15 +23,11 @@ return {
   -- Coding
   create_plugin 'preservim/tagbar', -- Display class + variable structure of file
   create_plugin 'preservim/nerdcommenter', -- :NERDCommenterToggle - Toggle comment code
-  --create_plugin 'fatih/vim-go', -- Already have nvim-lspconfig with gopls enabled
 
   -- Workflow
   create_plugin 'ojroques/vim-oscyank', -- Copy from ssh + tmux + docker to clipboard
   create_plugin 'mtdl9/vim-log-highlighting', -- Highlight log file
   create_plugin 'mg979/vim-visual-multi', -- Testing: Multiple select like vscode
-  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = ... },
-  --create_plugin 'gruvbox-community/gruvbox', -- gruv box community
-  --create_plugin 'morhetz/gruvbox', -- gruv box origin
   {
     -- Workspace for nvim, open on where you left at
     'thaerkh/vim-workspace',
@@ -36,6 +35,42 @@ return {
     config = function()
       vim.g.workspace_autocreate = 0 -- don't autocreate workspace when open new file
       vim.g.workspace_autosave = 0 -- disable auto save on InsertLeave
+    end,
+  },
+  {
+    'ibhagwan/fzf-lua',
+    -- optional for icon support
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    },
+    config = function()
+      -- calling `setup` is optional for customization
+      local fzflua = require 'fzf-lua'
+      local fzf_actions = require 'fzf-lua.actions'
+      fzflua.setup {
+        'telescope',
+        winopts = {
+          preview = {
+            layout = 'vertical',
+            vertical = 'up:75%',
+            winopts = { number = true },
+          },
+        },
+        lsp = {
+          jump_to_single_result = true,
+          jump_to_single_result_action = fzf_actions.file_edit,
+        },
+      }
+
+      vim.keymap.set('n', '<leader>sh', fzflua.helptags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', fzflua.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', fzflua.files, { desc = '[S]earch [F]ile' })
+      vim.keymap.set('n', '<leader>ss', fzflua.builtin, { desc = '[S]earch [S]elect Fzf' })
+      vim.keymap.set('n', '<leader>sw', fzflua.grep_cword, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', fzflua.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sr', fzflua.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sc', fzflua.colorschemes, { desc = '[S]earch [C]olorscheme' })
+      vim.keymap.set('n', '<C-f>', fzflua.live_grep, { desc = '[S]earch by [G]rep' })
     end,
   },
 
@@ -81,7 +116,7 @@ return {
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = 'make',
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    -- build = "" -- for windows
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
       'stevearc/dressing.nvim',
